47b3ef637d634af9c1a162fcc0b37018b5aa6f2e
Sid <68089579+BasicCorruption@users.noreply.github.com>
2021-12-20 14:26:44 -0500

    Update Parser.hx

--- source/Parser.hx
+++ source/Parser.hx
@@ -4,4 +4,10 @@ class Parser {
   public static function getArray(line:String) {
     return line.split(",");
   }
+  
+  public static function getValueInLine(line:String, pos:Int) {
+    var lineA rray = getArray(line);
+    
+    return lineArray[pos-1];
+  }
 }
