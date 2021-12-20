7cae83408e5ac93e2cc0b9ecfda1478576f87a6c
Sid <68089579+BasicCorruption@users.noreply.github.com>
2021-12-20 14:45:08 -0500

    Update Visualisation.hx

--- source/Visualisation.hx
+++ source/Visualisation.hx
@@ -3,6 +3,7 @@ package;
 class Visualisation extends FlxState {
   public var tick = 0;
   public var time = 0;
+  public var fTime = 0;
   public override function create() { super.create(); }
   public override function update(dt:Float) 
   {
@@ -10,5 +11,6 @@ class Visualisation extends FlxState {
     
     if (tick >= 1) { tick = 0; time+=1; }
     tick+=dt;
+    fTime+=dt;
   }
 }
