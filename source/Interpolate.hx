22ef7929000188b1c84118cd9ec1d2b98b51f50d
Sid <68089579+BasicCorruption@users.noreply.github.com>
2021-12-21 08:13:45 -0500

    Update Interpolate.hx

--- source/Interpolate.hx
+++ source/Interpolate.hx
@@ -2,7 +2,7 @@ package;
 
 class Interpolate
 {
-	public static function fromTwoNumbers(x1:Float, y1:Float, x2:Float, y2:Float, x)
+	public static function twoPoints(x1:Float, y1:Float, x2:Float, y2:Float, x:Float)
 	{
 		var y = (((y2 - y1) / (x2 - x1)) * (x - x1)) + y1;
 		return y;
